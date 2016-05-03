3b9c8bc1582d2ab7a06fdfa887c676851f02b5de
Josh Rendek <josh@bluescripts.net>
2016-05-02 22:48:17 -0400

    Add honeypot model and track honeypots

--- app/controllers/api/logins_controller.rb
+++ app/controllers/api/logins_controller.rb
@@ -3,7 +3,7 @@ class Api::LoginsController < ApplicationController
   def create
     body = JSON.parse(request.body.read)
     honeypot = Honeypot.find_or_create_by(ip: request.remote_addr)
-    honey.increment(:logins, 1)
+    honeypot.increment(:logins, 1)
     Login.create(body)
     LoginCount.inc(body['remote_addr'])
     render json: {status: :ok}, status: :ok
