c31bf3412d1b6bf412b4e4d2390cd181a7f60288
Josh Rendek <josh@bluescripts.net>
2016-08-21 14:29:19 -0400

    handle nil body

--- app/controllers/api/http_requests_controller.rb
+++ app/controllers/api/http_requests_controller.rb
@@ -2,7 +2,7 @@ class Api::HttpRequestsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def create
     body = Oj.load(request.body.read)
-    redner json: {status: :ok}, status: :ok and return if body.nil?
+    render json: {status: :ok}, status: :ok and return if body.nil?
     headers = body['headers'].map{|x| x.flatten.join(": ") }
     formdata =  body['form_data'].map{|x| { x[0] => x[1][0] } }
     HttpRequest.create(url: body['url'], response: body['response'].gsub("\u0000", ""),
