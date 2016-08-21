9f7991e146a82c359572516c9703ec0526faa0dd
Josh Rendek <josh@bluescripts.net>
2016-08-20 22:43:16 -0400

    Remove null bytes from response

--- app/controllers/api/http_requests_controller.rb
+++ app/controllers/api/http_requests_controller.rb
@@ -4,7 +4,7 @@ class Api::HttpRequestsController < ApplicationController
     body = Oj.load(request.body.read)
     headers = body['headers'].map{|x| x.flatten.join(": ") }
     formdata =  body['form_data'].map{|x| { x[0] => x[1][0] } }
-    HttpRequest.create(url: body['url'], response: body['response'],
+    HttpRequest.create(url: body['url'], response: body['response'].gsub("\u0000", ""),
                        hostname: body['hostname'], headers: headers,
                        formdata: formdata, method: body['method'], guid: body['guid'])
     render json: {status: :ok}, status: :ok
