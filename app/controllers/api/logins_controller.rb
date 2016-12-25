0f64b26326789bf4a253a895df0acb5256446bd3
Josh Rendek <josh@bluescripts.net>
2016-12-24 20:56:38 -0500

    Log HTTP responses for Proxies

--- app/controllers/api/logins_controller.rb
+++ app/controllers/api/logins_controller.rb
@@ -3,9 +3,9 @@ class Api::LoginsController < ApplicationController
   def create
     body = Oj.load(request.body.read)
     ip = request.env['HTTP_X_FORWARDED_FOR'] || request.remote_ip
-    Rails.logger.info "******************"
-    Rails.logger.info ip
-    Rails.logger.info "******************"
+    if ip.include?(',')
+      ip = ip.split(', ')[0]
+    end
     honeypot = Honeypot.find_or_create_by(ip: ip)
     honeypot.increment!(:logins, 1)
     Login.create(body)
